1641ea8c3a0c834aae80be3203a227b4f8bcf8cc
squaisser <sebastian.quaisser@rwth-aachen.de>
2026-01-20 10:06:10 +0100

    add initial conditions and max speed

--- src/equations/incompressible_euler_relaxation_2d.jl
+++ src/equations/incompressible_euler_relaxation_2d.jl
@@ -15,6 +15,45 @@ function varnames(::typeof(cons2cons), ::IncompressibleEulerRelaxationEquations2
     return ("p_eps", "v1", "v2", "V_eps_11", "V_eps_12", "V_eps_21", "V_eps_22")
 end
 
+function initial_condition_constant(x, t,
+                                  equations::IncompressibleEulerRelaxationEquations2D)
+    #TODO: assign proper initial condition
+    RealT = eltype(x)
+    p_eps = convert(RealT, 1)
+    v1 = convert(RealT, 1)
+    v2 = convert(RealT, 0)
+    V_eps_11 = convert(RealT, 1)
+    V_eps_12 = convert(RealT, 0)
+    V_eps_21 = convert(RealT, 0)
+    V_eps_22 = convert(RealT, 1)
+    return SVector(p_eps, v1, v2, V_eps_11, V_eps_12, V_eps_21, V_eps_22)
+end
+
+function initial_condition_riemann(x, t,
+                                  equations::IncompressibleEulerRelaxationEquations2D)
+    #TODO: assign proper initial condition
+    RealT = eltype(x)
+    v1_l = convert(RealT, 0)
+    v1_r = convert(RealT, 1)
+    v2_l = convert(RealT, 0)
+    v2_r = convert(RealT, 0)
+    p_eps_l = convert(RealT, 1)*equations.epsilon^2
+    p_eps_r = convert(RealT, 0.125)*equations.epsilon^2
+    V_eps_11_l = convert(RealT, 1)*equations.epsilon^2
+    V_eps_11_r = convert(RealT, 0.125)*equations.epsilon^2
+    V_eps_12_l = convert(RealT, 0)
+    V_eps_12_r = convert(RealT, 0)
+    V_eps_21_l = convert(RealT, 0)
+    V_eps_21_r = convert(RealT, 0)
+    V_eps_22_l = convert(RealT, 1)*equations.epsilon^2
+    V_eps_22_r = convert(RealT, 0.125)*equations.epsilon^2
+    if x[1] < 0.0
+        return SVector(p_eps_l, v1_l, v2_l, V_eps_11_l, V_eps_12_l, V_eps_21_l, V_eps_22_l)
+    else
+        return SVector(p_eps_r, v1_r, v2_r, V_eps_11_r, V_eps_12_r, V_eps_21_r, V_eps_22_r)
+    end
+end
+
 @inline function source_terms_homogeneous(u, x, t,
                               equations::IncompressibleEulerRelaxationEquations2D)
     s = zero(eltype(u))
@@ -55,6 +94,11 @@ end
         f7 = equations.a * v2
     end
     return SVector(f1, f2, f3, f4, f5, f6, f7)
+end
+
+@inline function max_abs_speed_naive(u_ll, u_rr, orientation::Integer,
+                               equations::IncompressibleEulerRelaxationEquations2D)
+    return sqrt(equations.a+1)/(equations.epsilon)
 end
 
 end # @muladd
